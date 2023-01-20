1ae0a8342832d572ffaf673328ca1cbb6eb5a170
oraking <mingyu.lee@linecorp.com>
2023-01-20 11:53:59 +0900

    settings

--- fastcampus/settings.py
+++ fastcampus/settings.py
@@ -77,10 +77,22 @@ WSGI_APPLICATION = 'fastcampus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project1', # DB명
+        'USER': 'admin', # 데이터베이스 계정
+        'PASSWORD': '1234qwer!@', # 계정 비밀번호
+        'HOST': '', # 데이테베이스 주소(IP)
+        'PORT': '3306', # 데이터베이스 포트(보통은 3306)
     }
 }
 
