ef9fded9e7317856721ba4baf4f82d21ee3090e7
oramyking <mingyu.lee@linecorp.com>
2023-01-24 21:46:31 +0900

    Update settings.py

--- fastcampus/settings.py
+++ fastcampus/settings.py
@@ -78,24 +78,24 @@ WSGI_APPLICATION = 'fastcampus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project1',  
+        'USER': 'admin', 
+        'PASSWORD': '1234qwer',  
+        'HOST': 'database-1.cgkoun7pqwlg.ap-northeast-2.rds.amazonaws.com', 
+        'PORT': '3306',  
     }
 }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'project1',  
-#         'USER': 'admin', 
-#         'PASSWORD': '1234qwer',  
-#         'HOST': 'database-1.cgkoun7pqwlg.ap-northeast-2.rds.amazonaws.com', 
-#         'PORT': '3306',  
-#     }
-# }
 
 
 # Password validation
